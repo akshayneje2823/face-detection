3a79bc7ad37fc7c0f8e24300345f6fd4902e4598
Akshay Neje <akshayneje2823@gmail.com>
2023-03-18 19:55:24 +0530

    Done

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,24 +3,24 @@ import './intro.css'
 
 function intro() {
   return (
-    <div className='intro'>
+    <div className='introduction'>
       <div className="intro-left">
-        <div className="intro-left-wrapper"> 
-          <h2 className="intro-i">Hello,My name is </h2>
+        <div className="intro-left-wrapper">
+          <h2 className="intro">Hello,My name is </h2>
           <h1 className='intro-name'>Akshay Neje</h1>
           <div className="intro-title">
-            <div className="i-title-wrapper">
-              <div className="i-title-item">Web Developer</div>
-              <div className="i-title-item">UI/UX Developer</div>
-              <div className="i-title-item">Photographer</div>
-              <div className="i-title-item">Content writer</div>
+            <div className="intro-title-wrapper">
+              <div className="intro-title-item">Web Developer</div>
+              <div className="intro-title-item">UI/UX Developer</div>
+              <div className="intro-title-item">Photographer</div>
+              <div className="intro-title-item">Content writer</div>
             </div>
           </div>
         </div>
       </div>
       <div className="intro-right">right</div>
     </div>
-  )
+  ) 
 }
 
 export default intro
